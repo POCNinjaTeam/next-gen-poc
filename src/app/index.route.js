0d9768e84c9c370a6e8bea2b747fb3ed88ebe9f9
Bruno Trigueiro <btrigueiro@gmail.com>
2016-01-11 13:40:51 -0200

    beautify

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,15 +1,25 @@
+'use strict';
+
+
 export function routerConfig($componentLoaderProvider) {
-  'ngInject';
-  $componentLoaderProvider.setTemplateMapping(function(name) {
-    return `app/${ name }/${ name }.html`;
-  });
+    'ngInject';
+
+    $componentLoaderProvider.setTemplateMapping(function (name) {
+        return `app/${ name }/${ name }.html`;
+    });
 }
 
+
+
 export class RouterController {
-  constructor($router) {
-    'ngInject';
-    $router.config([
-      { path: '/', component: 'main' }
-    ]);
-  }
+    constructor($router) {
+        'ngInject';
+
+        $router.config([
+            {
+                path: '/',
+                component: 'main'
+            }
+        ]);
+    }
 }
