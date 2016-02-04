c0004d2ac0cdc7e70945c1104fcd34e1362e9026
Bruno Trigueiro <btrigueiro@gmail.com>
2016-02-04 12:31:18 -0300

    adding a typescript example app

--- src/app/bootstrap.ts
+++
@@ -1,5 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import TodoApp from './components/todo-app/component'
-import {TodoStore} from './components/todo-app/service';
-
-bootstrap(TodoApp, [TodoStore]);
